f8111ceddfaab53af493279dbca7764fa7ab5241
JerkoMintHome <jerome.potie@gmail.com>
2021-05-10 20:20:27 +0200

    Improve slider, display of the pictures, add a flavicon, and the page title

--- src/elements/tab/TabTwo.jsx
+++ src/elements/tab/TabTwo.jsx
@@ -1,86 +1,126 @@
-import React from "react"
-import {Tab, Tabs, TabList, TabPanel} from 'react-tabs'
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
-import ProgressBdd from "../../blocks/progressbar/ProgressBdd"
-import ProgressMain from "../../blocks/progressbar/ProgressMain"
-import ProgressLanguage from "../../blocks/progressbar/ProgressLanguage"
-import ProgressOther from "../../blocks/progressbar/ProgressOther"
+import ProgressBdd from "../../blocks/progressbar/ProgressBdd";
+import ProgressMain from "../../blocks/progressbar/ProgressMain";
+import ProgressLanguage from "../../blocks/progressbar/ProgressLanguage";
+import ProgressOther from "../../blocks/progressbar/ProgressOther";
 
 const TabsTwo = (props) => {
+  const [tab1, tab2, tab3, tab4, tab5] = [
+    "En vedette",
+    "Langages",
+    "Base de données",
+    "Autres",
+    "Mon parcours",
+  ];
 
-    const
-        tab1 = "En vedette",
-        tab2 = "Langages",
-        tab3 = "Base de données",
-        tab4 = "Autres",
-        tab5 = "Mon parcours"
+  const { tabStyle } = props;
 
-    const {tabStyle} = props
+  return (
+    <div>
+      {/* Start Tabs Area */}
+      <div className="tabs-area">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 ">
+              <Tabs>
+                <TabList className={`${tabStyle}`}>
+                  <Tab>{tab1}</Tab>
+                  <Tab>{tab2}</Tab>
+                  <Tab>{tab3}</Tab>
+                  <Tab>{tab4}</Tab>
+                  <Tab>{tab5}</Tab>
+                </TabList>
 
-    return (
-        <div>
-            {/* Start Tabs Area */}
-            <div className="tabs-area">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12 ">
-                            <Tabs>
-                                <TabList className={`${tabStyle}`}>
-                                    <Tab>{tab1}</Tab>
-                                    <Tab>{tab2}</Tab>
-                                    <Tab>{tab3}</Tab>
-                                    <Tab>{tab4}</Tab>
-                                    <Tab>{tab5}</Tab>
-                                </TabList>
+                <TabPanel>
+                  <ProgressMain />
+                </TabPanel>
 
-                                <TabPanel>
-                                    <ProgressMain />
-                                </TabPanel>
+                <TabPanel>
+                  <ProgressLanguage />
+                </TabPanel>
 
-                                <TabPanel>
-                                    <ProgressLanguage />
-                                </TabPanel>
+                <TabPanel>
+                  <ProgressBdd />
+                </TabPanel>
 
-                                <TabPanel>
-                                    <ProgressBdd />
-                                </TabPanel>
+                <TabPanel>
+                  <ProgressOther />
+                </TabPanel>
 
-                                <TabPanel>
-                                    <ProgressOther />
-                                </TabPanel>
-
-                                <TabPanel>
-                                    <div className="single-tab-content">
-                                        <ul>
-                                            <li>
-                                                <a href="https://bluesquare.io/" target="_blank" rel="noopener noreferrer">
-                                                    Bluesquare.io </a>
-                                                    - Développeur Web / Web Mobile (stage)
-                                                <br />
-                                                    [Septembre à Décembre 2020]<br />
-                                                    ✠ <small>Refonte d'un site français reconnu de chasseur de têtes = [ <b>SCSS</b> / <b>ReactJs</b> / <b>Laravel</b> ] => Implémenter de nouvelles fonctionnalités dynamiques en React, créer des animations, améliorer et déboguer l'interface graphique.</small><br />
-                                                    ✠ <small>R&D = Développer un centre d'aide pour les clients de l'agence => [<b>PHP</b>, <b>Laravel</b>, <b>InertiaJs</b>, <b>VueJs</b>, <b>TailWindCSS</b>]</small>
-                                            </li>
-                                            <li>
-                                                <a href="https://www.wildcodeschool.com/fr-FR" target="_blank" rel="noopener noreferrer">
-                                                    Wild Code School </a>
-                                                    - Formation au titre professionnel de développeur Web / Web Mobile
-                                                <br />
-                                                    [Mars à Juillet 2020]
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </TabPanel>
-                            </Tabs>
-                        </div>
-                    </div>
-                </div>
+                <TabPanel>
+                  <div className="single-tab-content">
+                    <ul>
+                      <li>
+                        [Mai à Août 2021]
+                        <br />
+                        <a
+                          href="https://simplon.co/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Simplon.co{" "}
+                        </a>
+                        - Formation développeur back-end PHP Symfony
+                      </li>
+                      <li>
+                        [Mars 2021]
+                        <br />
+                        Obtention du titre de
+                        <b> développeur Web / Web Mobile</b>
+                      </li>
+                      <li>
+                        [Septembre à Décembre 2020]
+                        <br />
+                        <a
+                          href="https://bluesquare.io/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Bluesquare.io{" "}
+                        </a>
+                        - Développeur Web / Web Mobile (stage)
+                        <br />✠{" "}
+                        <small>
+                          Refonte d'un site français reconnu de chasseur de
+                          têtes = [ <b>SCSS</b> / <b>ReactJs</b> /{" "}
+                          <b>Laravel</b> ] => Implémenter de nouvelles
+                          fonctionnalités dynamiques en React, créer des
+                          animations, améliorer et déboguer l'interface
+                          graphique.
+                        </small>
+                        <br />✠{" "}
+                        <small>
+                          R&D = Développer un centre d'aide pour les clients de
+                          l'agence => [<b>PHP</b>, <b>Laravel</b>,{" "}
+                          <b>InertiaJs</b>, <b>VueJs</b>, <b>TailWindCSS</b>]
+                        </small>
+                      </li>
+                      <li>
+                        [Mars à Juillet 2020]
+                        <br />
+                        <a
+                          href="https://www.wildcodeschool.com/fr-FR"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Wild Code School{" "}
+                        </a>
+                        - Formation au titre professionnel de développeur Web /
+                        Web Mobile
+                      </li>
+                    </ul>
+                  </div>
+                </TabPanel>
+              </Tabs>
             </div>
-            {/* End Tabs Area */}
+          </div>
         </div>
-    )
-}
+      </div>
+      {/* End Tabs Area */}
+    </div>
+  );
+};
 
-
-
-export default TabsTwo
+export default TabsTwo;
--- src/home/PersonalPortfolio.jsx
+++ src/home/PersonalPortfolio.jsx
@@ -1,137 +1,171 @@
-import React, {Component, Fragment} from "react"
+import React, { Component, Fragment } from "react";
 
-import Helmet from "../component/common/Helmet"
-import {FiChevronUp} from "react-icons/fi"
-import ScrollableAnchor, {configureAnchors} from 'react-scrollable-anchor'
-import ScrollToTop from 'react-scroll-up'
-import Typed from 'typed.js'
+import Helmet from "../component/common/Helmet";
+import { FiChevronUp } from "react-icons/fi";
+import ScrollableAnchor, { configureAnchors } from "react-scrollable-anchor";
+import ScrollToTop from "react-scroll-up";
+import Typed from "typed.js";
 
-import ContactOne from "../elements/contact/ContactOne"
-import Header from "../component/header/HeaderThree"
-import PortfolioList from "../elements/portfolio/PortfolioList"
-import TabTwo from "../elements/tab/TabTwo"
+import ContactOne from "../elements/contact/ContactOne";
+import Header from "../component/header/HeaderThree";
+import PortfolioList from "../elements/portfolio/PortfolioList";
+import TabTwo from "../elements/tab/TabTwo";
 
 // Offset all anchors by -60 to account for a fixed header
 // and scroll more quickly than the default 400ms
-configureAnchors({offset: -60, scrollDuration: 2000})
+configureAnchors({ offset: -60, scrollDuration: 2000 });
 class PersonalPortfolio extends Component {
+  state = {
+    aboutImg: this.getAboutImage(),
+  };
 
-    state = {
-        aboutImg: this.getAboutImage()
-    }
+  componentDidMount() {
+    const options = {
+      strings: ["Mobile", "Fullstack"],
+      typeSpeed: 125,
+      backSpeed: 50,
+      startDelay: 1000,
+      backDelay: 2500,
+      smartBackspace: true,
+      loop: true,
+      loopCount: Infinity,
+      showCursor: false,
+      fadeOut: true,
+      fadeOutClass: "typed-fade-out",
+      fadeOutDelay: 500,
+    };
+    // this.el refers to the <span> in the render() method
+    this.typed = new Typed(this.el, options);
+  }
 
-    componentDidMount () {
-        const options = {
-            strings: ['Mobile', 'Fullstack'],
-            typeSpeed: 125,
-            backSpeed: 50,
-            startDelay: 1000,
-            backDelay: 2500,
-            smartBackspace: true,
-            loop: true,
-            loopCount: Infinity,
-            showCursor: false,
-            fadeOut: true,
-            fadeOutClass: 'typed-fade-out',
-            fadeOutDelay: 500,
-        }
-        // this.el refers to the <span> in the render() method
-        this.typed = new Typed(this.el, options)
-    }
+  getAboutImage() {
+    const prefix = "/assets/images/about/shooting_";
+    const random = Math.ceil(Math.random() * 11);
+    return `${prefix}${random}.jpg`;
+  }
 
-    getAboutImage() {
-        const prefix = '/assets/images/about/shooting_'
-        const random = Math.ceil(Math.random() * 11)
-        return `${prefix}${random}.jpg`
-    }
+  render() {
+    let title = "À propos de moi",
+      description =
+        "En reconversion dans le domaine du développement Web et Web mobile, j'ai effectué une formation intensive à la Wild Code School, école spécialisée dans le numérique dans laquelle j'ai fait mes premières armes en Javascript (React / NodeJs). À la suite à mon stage dans l'agence Bluesquare.io, et après avoir obtenu mon titre professionnel, je suis actuellement une nouvelle formation de 3 mois en PHP / Symfony à Simplon.co.";
 
-    render () {
-        let title = 'À propos de moi',
-            description = "En reconversion dans le domaine du développement Web et Web mobile, j'ai suivi une formation intensive à la Wild Code School, école spécialisée dans les métiers de la tech et du numérique. Aujourd'hui, suite à mon stage dans l'agence Bluesquare.io, je suis en recherche active afin de décrocher mon premier job dans ma nouvelle vie de dev."
+    const SlideList = [
+      {
+        textPosition: "text-left",
+        category: "Hello world",
+        title: "Bonjour, moi c'est Jérôme !<br />",
+        transition: "je suis un .....",
+        title2: `<span>Développeur Web <span />`,
+        description:
+          "Je suis attiré par le domaine de l'informatique depuis mon plus jeune âge. Après un début de carrière dans la restauration et la livraison, j'ai décidé de me donner les moyens de réaliser mon rêve de gamin... faire du développement mon métier !",
+        buttonText: "",
+        buttonLink: "",
+      },
+    ];
 
-        const SlideList = [
-            {
-                textPosition: 'text-left',
-                category: 'Hello world',
-                title: 'Bonjour, moi c\'est Jérôme !<br />',
-                transition: 'je suis un .....',
-                title2: `<span>Développeur Web <span />`,
-                description: "Je suis attiré par le domaine de l'informatique depuis mon plus jeune âge. Après un début de carrière dans la restauration et la livraison, j'ai décidé de me donner les moyens de réaliser mon rêve de gamin... faire du développement mon métier !",
-                buttonText: '',
-                buttonLink: ''
-            }
-        ]
+    return (
+      <Fragment>
+        <Helmet pageTitle="Personal Portfolio" />
+        <Header logo="symbol-light" color="color-black" />
 
-        return (
-            <Fragment>
-                <Helmet pageTitle="Personal Portfolio" />
-                    <Header logo="symbol-light" color="color-black" />
-
-                {/* Start Slider Area   */}
-                <div className="slider-wrapper">
-                    {/* Start Single Slide */}
-                    {SlideList.map((value, index) => (
-                        <div className="slide personal-portfolio-slider slider-paralax slider-style-3 d-flex align-items-center justify-content-center bg_image bg_image--33" key={index}>
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col-lg-12 ">
-                                        <div className={`inner ${value.textPosition}`}>
-                                            {value.category ? <span>{value.category}</span> : ''}
-                                            {value.title ? <h1 className="title" dangerouslySetInnerHTML={{__html: value.title}}></h1> : ''}
-                                            {value.transition ? <span>{value.transition}</span> : ''}
-                                            {value.title2 ?
-                                                <div className="">
-                                                    <h1 className="title d-inline" dangerouslySetInnerHTML={{__html: value.title2}} />
-                                                    <h1 className='title d-inline'>
-                                                        <span
-                                                            ref={(el) => {this.el = el}}
-                                                        />
-                                                    </h1>
-                                                </div>
-                                                : ''}
-                                            {value.description ? <p className="description">{value.description}</p> : ''}
-                                            {value.buttonText ? <div className="slide-btn"><a className="rn-button-style--2 btn-primary-color" href={`${value.buttonLink}`}>{value.buttonText}</a></div> : ''}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+        {/* Start Slider Area   */}
+        <div className="slider-wrapper">
+          {/* Start Single Slide */}
+          {SlideList.map((value, index) => (
+            <div
+              className="slide personal-portfolio-slider slider-paralax slider-style-3 d-flex align-items-center justify-content-center bg_image bg_image--33"
+              key={index}
+            >
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-12 ">
+                    <div className={`inner ${value.textPosition}`}>
+                      {value.category ? <span>{value.category}</span> : ""}
+                      {value.title ? (
+                        <h1
+                          className="title"
+                          dangerouslySetInnerHTML={{ __html: value.title }}
+                        ></h1>
+                      ) : (
+                        ""
+                      )}
+                      {value.transition ? <span>{value.transition}</span> : ""}
+                      {value.title2 ? (
+                        <>
+                          <h1
+                            className="title d-inline"
+                            dangerouslySetInnerHTML={{ __html: value.title2 }}
+                          />
+                          <h1 className="title d-inline">
+                            <span
+                              className="text-anim"
+                              ref={(el) => {
+                                this.el = el;
+                              }}
+                            />
+                          </h1>
+                        </>
+                      ) : (
+                        ""
+                      )}
+                      {value.description ? (
+                        <p className="description">{value.description}</p>
+                      ) : (
+                        ""
+                      )}
+                      {value.buttonText ? (
+                        <div className="slide-btn">
+                          <a
+                            className="rn-button-style--2 btn-primary-color"
+                            href={`${value.buttonLink}`}
+                          >
+                            {value.buttonText}
+                          </a>
                         </div>
-                    ))}
-                    {/* End Single Slide */}
-                </div>
-                {/* End Slider Area   */}
-
-                {/* Start About Area */}
-                <ScrollableAnchor id={'about'}>
-                    <div className="about-area about-position-top pb--60  bg_color--3">
-                        <div className="about-wrapper">
-                            <div className="container">
-                                <div className="row row--20 align-items-end ">
-                                    <div className="col-lg-5 justify-content-center align-items-end">
-                                        <div className="thumbnail">
-                                            <img className="w-100" src={this.state.aboutImg} alt="About Images" />
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-7">
-                                        <div className="about-inner inner pt--100">
-                                            <div className="section-title">
-                                                <h2 className="title">{title}</h2>
-                                                <p className="description">{description}</p>
-                                            </div>
-                                            <div className="row mt--30 tab-container">
-                                                <TabTwo tabStyle="tab-style--1" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                      ) : (
+                        ""
+                      )}
                     </div>
-                </ScrollableAnchor>
-                {/* End About Area */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+          {/* End Single Slide */}
+        </div>
+        {/* End Slider Area   */}
 
-                {/* Start Brand Area */}
-                {/* <div className="rn-brand-area pb--90 bg_color--1">
+        {/* Start About Area */}
+        <ScrollableAnchor id={"about"}>
+          <div className="about-area about-position-top pb--60  bg_color--3">
+            <div className="about-wrapper">
+              <div className="container">
+                <div className="row row--20">
+                  <div className="col-lg-5 justify-content-center align-items-end">
+                    <div className="thumbnail">
+                      <img src={this.state.aboutImg} alt="About Images" />
+                    </div>
+                  </div>
+                  <div className="col-lg-7">
+                    <div className="about-inner inner pt--100">
+                      <div className="section-title">
+                        <h2 className="title">{title}</h2>
+                        <p className="description">{description}</p>
+                      </div>
+                      <div className="row mt--30 tab-container">
+                        <TabTwo tabStyle="tab-style--1" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </ScrollableAnchor>
+        {/* End About Area */}
+
+        {/* Start Brand Area */}
+        {/* <div className="rn-brand-area pb--90 bg_color--1">
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12">
@@ -148,58 +182,65 @@ class PersonalPortfolio extends Component {
                         </div>
                     </div>
                 </div> */}
-                {/* End Brand Area */}
+        {/* End Brand Area */}
 
-                {/* Start Portfolio Area */}
-                <ScrollableAnchor id={'project'}>
-                    <div className="portfolio-area pb--60 bg_color--7">
-                        <div className="portfolio-sacousel-inner mb--55 mb_sm--0">
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col-lg-12">
-                                        <div className="section-title text-center service-style--3 mb--30 mb_sm--0">
-                                            <h2 className="title mt--100">Mes projets</h2>
-                                            <p>Découvrez ici mes dernières réalisations, ainsi que les projets sur lesquels j'ai été amené à collaborer.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <PortfolioList stylevariation="text-center mt--40" column="col-lg-4 col-md-6 col-sm-6 col-12" item="6" />
-                                </div>
-                                {/* <div className="row">
+        {/* Start Portfolio Area */}
+        <ScrollableAnchor id={"project"}>
+          <div className="portfolio-area pb--60 bg_color--7">
+            <div className="portfolio-sacousel-inner mb--55 mb_sm--0">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="section-title text-center service-style--3 mb--30 mb_sm--0">
+                      <h2 className="title mt--100">Mes projets</h2>
+                      <p>
+                        Découvrez ici mes dernières réalisations, ainsi que les
+                        projets sur lesquels j'ai été amené à collaborer.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <PortfolioList
+                    stylevariation="text-center mt--40"
+                    column="col-lg-4 col-md-6 col-sm-6 col-12"
+                    item="6"
+                  />
+                </div>
+                {/* <div className="row">
                                     <div className="col-lg-12">
                                         <div className="view-more-btn mt--60 mt_sm--30 text-center">
                                             <a className="rn-button-style--2 btn-solid" href="/blog"><span>En voir plus</span></a>
                                         </div>
                                     </div>
                                 </div> */}
-                            </div>
-                        </div>
-                    </div>
-                </ScrollableAnchor>
-                {/* End Portfolio Area */}
+              </div>
+            </div>
+          </div>
+        </ScrollableAnchor>
+        {/* End Portfolio Area */}
 
-                {/* Start Portfolio Area */}
+        {/* Start Portfolio Area */}
 
-                <ScrollableAnchor id={'contact'}>
-                    <div className="portfolio-area pb--120 bg_color--1">
-                        <ContactOne />
-                    </div>
-                </ScrollableAnchor>
-                {/* End Portfolio Area */}
+        <ScrollableAnchor id={"contact"}>
+          <div className="portfolio-area pb--120 bg_color--1">
+            <ContactOne />
+          </div>
+        </ScrollableAnchor>
+        {/* End Portfolio Area */}
 
-                {/* <FooterTwo /> */}
+        {/* <FooterTwo /> */}
 
-                {/* Start Back To Top */}
-                <div className="backto-top">
-                    <ScrollToTop showUnder={160} duration={1500} >
-                        <FiChevronUp />
-                    </ScrollToTop>
-                </div>
-                {/* End Back To Top */}
-            </Fragment>
-        )
-    }
+        {/* Start Back To Top */}
+        <div className="backto-top">
+          <ScrollToTop showUnder={160} duration={1500}>
+            <FiChevronUp />
+          </ScrollToTop>
+        </div>
+        {/* End Back To Top */}
+      </Fragment>
+    );
+  }
 }
 
-export default PersonalPortfolio
+export default PersonalPortfolio;

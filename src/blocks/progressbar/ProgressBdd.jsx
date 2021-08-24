26ffa143cc5cdd0d7ae3232a18960fc15e263329
jerkoLegion <jerome.potie@gmail.com>
2021-08-24 18:06:41 +0200

    Create a exp bar for my competence and modify the techno values

--- src/blocks/progressbar/ProgressBdd.jsx
+++ src/blocks/progressbar/ProgressBdd.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import {ProgressBar} from 'react-bootstrap'
+import ExpBar from './ExpBar'
 
 const ProgressMain = (props) => {
     return (
         // Start Single Progressbar
-        <div className={`rn-progress-bar ${props.ProgressStyle}`}>
+        <>
+            <div className={`rn-progress-bar ${props.ProgressStyle}`}>
 
-            <div className="single-progress custom-color--2">
-                <h6 className="title">MySql</h6>
-                <ProgressBar now={65} />
-                <span className="label">65%</span>
+                <div className="single-progress custom-color--2">
+                    <h6 className="title">MySql</h6>
+                    <ProgressBar now={50} />
+                </div>
+
+                <div className="single-progress custom-color--4">
+                    <h6 className="title">MongoDB</h6>
+                    <ProgressBar now={6} />
+                </div>
+
+            <ExpBar />
             </div>
-
-            <div className="single-progress custom-color--4">
-                <h6 className="title">MongoDB</h6>
-                <ProgressBar now={20} />
-                <span className="label">20%</span>
-            </div>
-
-        </div>
+        </>
         // {/* // End Progress Bar */}
     )
 }
--- src/blocks/progressbar/ProgressMain.jsx
+++ src/blocks/progressbar/ProgressMain.jsx
@@ -1,42 +1,40 @@
 import React from 'react'
-import {ProgressBar} from 'react-bootstrap'
+import { ProgressBar } from 'react-bootstrap'
+import ExpBar from './ExpBar'
 
 const ProgressMain = (props) => {
     return (
         // Start Single Progressbar
-        <div className={`rn-progress-bar ${props.ProgressStyle}`}>
+        <>
+            <div className={`rn-progress-bar ${props.ProgressStyle}`}>
 
-            <div className="single-progress custom-color--1">
-                <h6 className="title">React Js</h6>
-                <ProgressBar now={75} />
-                <span className="label">75%</span>
+                <div className="single-progress custom-color--1">
+                    <h6 className="title">React Js</h6>
+                    <ProgressBar now={53} />
+                </div>
+
+                <div className="single-progress custom-color--2">
+                    <h6 className="title">Node JS</h6>
+                    <ProgressBar now={40} />
+                </div>
+
+                <div className="single-progress custom-color--3">
+                    <h6 className="title">Vue Js</h6>
+                    <ProgressBar now={25} />
+                </div>
+
+                <div className="single-progress custom-color--5">
+                    <h6 className="title">Laravel</h6>
+                    <ProgressBar now={35} />
+                </div>
+
+                <div className="single-progress custom-color--4">
+                    <h6 className="title">Symfony</h6>
+                    <ProgressBar now={40} />
+                </div>
             </div>
-
-            <div className="single-progress custom-color--2">
-                <h6 className="title">Node JS</h6>
-                <ProgressBar now={70} />
-                <span className="label">70%</span>
-            </div>
-
-            <div className="single-progress custom-color--3">
-                <h6 className="title">Vue Js</h6>
-                <ProgressBar now={65} />
-                <span className="label">65%</span>
-            </div>
-
-            <div className="single-progress custom-color--1">
-                <h6 className="title">Laravel</h6>
-                <ProgressBar now={50} />
-                <span className="label">50%</span>
-            </div>
-
-            <div className="single-progress custom-color--1">
-                <h6 className="title">Symfony</h6>
-                <ProgressBar now={50} />
-                <span className="label">50%</span>
-            </div>
-
-        </div>
+            <ExpBar />
+        </>
         // {/* // End Progress Bar */}
     )
 }

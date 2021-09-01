e3395186ff02783c8c2508be468c90c6a406ff6e
jerkoLegion <jerome.potie@gmail.com>
2021-09-01 11:25:22 +0200

    formatting the new project form with fieldsets

--- src/portfolio/components/dashboard/pages/projects/ProjectForm.jsx
+++ src/portfolio/components/dashboard/pages/projects/ProjectForm.jsx
@@ -68,127 +68,127 @@ const ProjectForm = () => {
     <div className="project-form-container">
       <h2>New project</h2>
       <form onSubmit={submitForm}>
-        {/* Title */}
-        <Input
-          errors={formErrors.title}
-          id="title"
-          isRequired
-          label="titre"
-          placeholder="titre du projet"
-          setValue={(e) => handleForm(e)}
-          value={formDatas.title}
-        />
-        {/* short description */}
-        <Input
-          errors={formErrors.short_description}
-          id="short_description"
-          isRequired
-          label="courte description"
-          placeholder="description courte du projet (Apparaît dans les vignettes)"
-          setValue={(e) => handleForm(e)}
-          value={formDatas.short_description}
-        />
-        {/* Context */}
-        <Input
-          errors={formErrors.context}
-          id="context"
-          isRequired
-          label="Contexte"
-          placeholder="Entreprise ou école ou a été réalisé le projet"
-          setValue={(e) => handleForm(e)}
-          value={formDatas.context}
-        />
-        {/* Context url */}
-        <Input
-          errors={formErrors.context_url}
-          id="context_url"
-          label="Lien de l'établissement"
-          placeholder="Lien de l'établissement du contexte"
-          type="url"
-          setValue={(e) => handleForm(e)}
-          value={formDatas.context_url}
-        />
-        {/* github url */}
-        <Input
-          errors={formErrors.url_github}
-          id="url_github"
-          label="Lien vers le dépôt github"
-          placeholder="Insérer le lien du dépôt github"
-          type="url"
-          setValue={(e) => handleForm(e)}
-          value={formDatas.url_github}
-        />
-        {/* project preview url */}
-        <Input
-          errors={formErrors.url_test}
-          id="url_test"
-          label="Lien vers la page de test"
-          placeholder="Insérer le lien de la page de test"
-          type="url"
-          setValue={(e) => handleForm(e)}
-          value={formDatas.url_test}
-        />
-        {/* project date */}
-        <Input
-          errors={formErrors.date}
-          id="date"
-          isRequired
-          label="Date du projet"
-          type="date"
-          setValue={(e) => handleForm(e)}
-          value={formDatas.date}
-        />
-        {/* image prefix  */}
-        <Input
-          errors={formErrors.img_prefix}
-          id="img_prefix"
-          isRequired
-          label="Préfixe des images (même nom que le dossier du projet)"
-          placeholder="Ajouter un prefix"
-          setValue={(e) => handleForm(e)}
-          value={formDatas.img_prefix}
-        />
-        {/* thumbmail images background color */}
-        <Input
-          errors={formErrors.background}
-          id="background"
-          label="Couleur de fond (Sera utilisé pour le carousel d'image)"
-          type="color"
-          setValue={(e) => handleForm(e)}
-          value={formDatas.background}
-        />
-        {/* thumbmail images background color */}
-        <Input
-          errors={formErrors.nb_images}
-          id="nb_images"
-          label="Nombres d'images"
-          type="number"
-          setValue={(e) => handleForm(e)}
-          value={formDatas.nb_images}
-          min={0}
-          max={20}
-        />
-        {/* Active */}
-        <label className="switch">
-          <input
-            type="checkbox"
-            checked={formDatas.active && "checked"}
-            id="active"
-            onChange={() =>
-              setFormDatas({ ...formDatas, active: !formDatas.active })
-            }
+        <fieldset>
+          <legend>Informations générales</legend>
+          {/* project date */}
+          <Input
+            errors={formErrors.date}
+            id="date"
+            isRequired
+            label="Date du projet"
+            type="date"
+            setValue={(e) => handleForm(e)}
+            value={formDatas.date}
           />
-          <span className="slider round"></span>
-        </label>
-        <label htmlFor="active">Publier le projet ?</label>
-        {/* Description */}
-        <MdEditor
-          value={mdDescription}
-          setValue={setMdDescription}
-          errors={formErrors.description}
-          label="description du projet"
-          isRequired
-        />
+          {/* Title */}
+          <Input
+            errors={formErrors.title}
+            id="title"
+            isRequired
+            label="titre"
+            placeholder="titre du projet"
+            setValue={(e) => handleForm(e)}
+            value={formDatas.title}
+          />
+          {/* short description */}
+          <Input
+            errors={formErrors.short_description}
+            id="short_description"
+            isRequired
+            label="courte description"
+            placeholder="description courte du projet (Apparaît dans les vignettes)"
+            setValue={(e) => handleForm(e)}
+            value={formDatas.short_description}
+          />
+          {/* Description */}
+          <MdEditor
+            value={mdDescription}
+            setValue={setMdDescription}
+            errors={formErrors.description}
+            label="description du projet"
+            isRequired
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Contexte du projet</legend>
+          {/* Context */}
+          <Input
+            errors={formErrors.context}
+            id="context"
+            isRequired
+            label="Contexte"
+            placeholder="Entreprise ou école ou a été réalisé le projet"
+            setValue={(e) => handleForm(e)}
+            value={formDatas.context}
+          />
+          {/* Context url */}
+          <Input
+            errors={formErrors.context_url}
+            id="context_url"
+            label="Lien de l'établissement"
+            placeholder="Lien de l'établissement du contexte"
+            type="url"
+            setValue={(e) => handleForm(e)}
+            value={formDatas.context_url}
+          />
+        </fieldset>
+        <fieldset className="multiple-fields-wrapper">
+          <legend>Liens du projet</legend>
+          {/* github url */}
+          <Input
+            errors={formErrors.url_github}
+            id="url_github"
+            label="Lien vers le dépôt github"
+            placeholder="Insérer le lien du dépôt github"
+            type="url"
+            setValue={(e) => handleForm(e)}
+            value={formDatas.url_github}
+          />
+          {/* project preview url */}
+          <Input
+            errors={formErrors.url_test}
+            id="url_test"
+            label="Lien vers la page de test"
+            placeholder="Insérer le lien de la page de test"
+            type="url"
+            setValue={(e) => handleForm(e)}
+            value={formDatas.url_test}
+          />
+        </fieldset>
+        <fieldset className="multiple-fields-wrapper">
+          <legend>Images</legend>
+          {/* image prefix  */}
+          <Input
+            errors={formErrors.img_prefix}
+            id="img_prefix"
+            isRequired
+            label="Préfixe des images"
+            placeholder="Ajouter un prefix"
+            setValue={(e) => handleForm(e)}
+            value={formDatas.img_prefix}
+          />
+          {/* thumbmail images background color */}
+          <Input
+            errors={formErrors.background}
+            id="background"
+            label="Couleur de fond carousel d'images"
+            type="color"
+            setValue={(e) => handleForm(e)}
+            value={formDatas.background}
+          />
+          {/* thumbmail images background color */}
+          <Input
+            errors={formErrors.nb_images}
+            id="nb_images"
+            isRequired
+            label="Nombres d'images"
+            type="number"
+            setValue={(e) => handleForm(e)}
+            value={formDatas.nb_images}
+            min={0}
+            max={20}
+          />
+        </fieldset>
         <fieldset>
           <legend>Technos utilisées dans le projet</legend>
           <ul className="techno-wrapper">
@@ -209,6 +209,19 @@ const ProjectForm = () => {
               })}
           </ul>
         </fieldset>
+        {/* Active */}
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={formDatas.active && "checked"}
+            id="active"
+            onChange={() =>
+              setFormDatas({ ...formDatas, active: !formDatas.active })
+            }
+          />
+          <span className="slider round"></span>
+        </label>
+        <label htmlFor="active">Publier le projet ?</label>
       </form>
     </div>
   );

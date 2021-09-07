3c03dd2fe1fc9924364e9a721ad6eabc9f285e61
jerkoLegion <jerome.potie@gmail.com>
2021-09-07 11:47:31 +0200

    Fetch projects in database and display the table header with categories

--- src/portfolio/components/dashboard/pages/projects/ProjectList.jsx
+++ src/portfolio/components/dashboard/pages/projects/ProjectList.jsx
@@ -1,16 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import axios from "axios";
+import ToasterDisplay from "../../../../helpers/ToasterDisplay";
 
 const ProjectList = () => {
+  const [projects, setProjects] = useState({});
+
   useEffect(() => {
     axios
-      .get("/projects")
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
+      .get("/projects", {
+        headers: {
+          authorization: "Bearer: " + sessionStorage.getItem("token")
+        }
+      })
+      .then((res) => setProjects(res.data))
+      .catch(
+        (err) => console.log(err.response) || ToasterDisplay("Erreur lors de la récupération des projets", "fail")
+      );
   }, []);
-
-  return <div className="project-list">Liste des projets</div>;
+  console.log({ projects });
+  return (
+    <div className="project-list-container">
+      <h1>Liste des projets</h1>
+      <table>
+        <thead>
+          <tr className="project-main-categories">
+            <th rowSpan="2">Date</th>
+            <th colSpan="4">Informations principales</th>
+            <th colSpan="2">Contexte</th>
+            <th colSpan="3">Images</th>
+            <th rowSpan="2">Actif</th>
+          </tr>
+          <tr className="project-categories">
+            <th>Titre</th>
+            <th>Description Courte</th>
+            <th>Github</th>
+            <th>Url de test</th>
+            <th>Descriptif</th>
+            <th>Lien</th>
+            <th>Nb</th>
+            <th>Préfixe</th>
+            <th>Background</th>
+          </tr>
+        </thead>
+        {/* <tbody>{projects && projects.map((project) => {
+          const length = project.length
+          return (
+            tr>t
+          )
+        })}</tbody> */}
+      </table>
+    </div>
+  );
 };
 
 export default ProjectList;

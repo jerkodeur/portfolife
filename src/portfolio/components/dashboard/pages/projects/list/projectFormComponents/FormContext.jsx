c516cc59beea16a8d112e6d010cbf8273c97f3c4
jerkoLegion <jerome.potie@gmail.com>
2021-09-22 12:05:01 +0200

    Change the name of the file

--- src/portfolio/components/dashboard/pages/projects/list/projectFormComponents/FormContext.jsx
+++ src/portfolio/components/dashboard/pages/projects/list/projectFormComponents/FormContext.jsx
@@ -3,42 +3,56 @@ import React from "react";
 import propTypes from "prop-types";
 import Input from "../../../../../commons/forms/Input";
 
-const ContextForm = ({ formErrors, handleForm, formDatas }) => {
+const FormContext = ({ formErrors, handleForm, formDatas, keyPressHandler, updatedField }) => {
   const { context, contextUrl } = formDatas;
+  const { label, value, error } = updatedField;
+
+  formErrors[label] = updatedField && error;
+
   return (
-    <fieldset className={(formErrors.context || formErrors.contextUrl) && "error"}>
+    <fieldset
+      className={`multiple-fields-wrapper ${formErrors.context || formErrors.contextUrl ? "error" : undefined}`}
+    >
       <legend>Contexte du projet</legend>
       {/* Context */}
       <Input
+        onKeyPress={keyPressHandler}
         error={formErrors.context}
         id="context"
         isRequired
         label="Contexte"
         placeholder="Entreprise ou école ou a été réalisé le projet"
         setValue={(e) => handleForm(e)}
-        value={context}
+        value={label === "context" ? value : context}
       />
       {/* Context url */}
       <Input
+        onKeyPress={keyPressHandler}
         error={formErrors.contextUrl}
         id="contextUrl"
         label="Lien de l'établissement"
         placeholder="Lien de l'établissement du contexte"
         type="url"
         setValue={(e) => handleForm(e)}
-        value={contextUrl}
+        value={label === "contextUrl" ? value : contextUrl}
       />
     </fieldset>
   );
 };
 
-ContextForm.propTypes = {
+FormContext.defaultProps = {
+  formErrors: {},
+  updatedField: {}
+};
+
+FormContext.propTypes = {
   formDatas: propTypes.shape({
-    context: propTypes.string.isRequired,
+    context: propTypes.string,
     contextUrl: propTypes.string
   }).isRequired,
   formErrors: propTypes.objectOf(propTypes.string),
-  handleForm: propTypes.func.isRequired
+  handleForm: propTypes.func.isRequired,
+  updatedField: propTypes.shape()
 };
 
-export default ContextForm;
+export default FormContext;

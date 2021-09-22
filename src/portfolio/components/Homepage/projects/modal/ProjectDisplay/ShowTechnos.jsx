e1eb1e11d1cd95eb2f5fd78156a1f92774b867c4
jerkoLegion <jerome.potie@gmail.com>
2021-09-22 20:44:34 +0200

    Create a helper to chck if an image exist

--- src/portfolio/components/Homepage/projects/modal/ProjectDisplay/ShowTechnos.jsx
+++ src/portfolio/components/Homepage/projects/modal/ProjectDisplay/ShowTechnos.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
+import ToasterDisplay from "../../../../../helpers/ToasterDisplay";
+
+import { imageExist } from "../../../../../helpers/helpers";
+
 const ShowTechnos = ({ technos }) => (
   <div className="display-technos">
     {technos.map((techno, index) => {
-      const img = require(`../../../../../../../public/assets/images/technos/${techno.image_name}`);
-      return (
-        <img src={img} alt={techno.name} title={techno.name} key={index} />
-      );
+      let img = "";
+      try {
+        imageExist("technos", techno.image_name);
+        img = require(`../../../../../../../public/assets/images/technos/${techno.image_name}`);
+      } catch (e) {
+        img = require("../../../../../../../public/assets/images/technos/notfound.png");
+        ToasterDisplay("Image inconnue détectée", "fail", { position: "bottom-left", duration: 2500 });
+      }
+      return <img src={img} alt={techno.name} title={techno.name} key={index} />;
     })}
   </div>
 );

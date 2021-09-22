1ede260178fd4457c1a5b5629cbb8b79bd97b7eb
jerkoLegion <jerome.potie@gmail.com>
2021-09-22 20:52:47 +0200

    Change the title name if the image doesn't exsit

--- src/portfolio/components/Homepage/projects/modal/ProjectDisplay/ShowTechnos.jsx
+++ src/portfolio/components/Homepage/projects/modal/ProjectDisplay/ShowTechnos.jsx
@@ -8,14 +8,18 @@ const ShowTechnos = ({ technos }) => (
   <div className="display-technos">
     {technos.map((techno, index) => {
       let img = "";
+      let title = "";
+
       try {
         imageExist("technos", techno.image_name);
         img = require(`../../../../../../../public/assets/images/technos/${techno.image_name}`);
+        title = techno.name;
       } catch (e) {
         img = require("../../../../../../../public/assets/images/technos/notfound.png");
         ToasterDisplay("Image inconnue détectée", "fail", { position: "bottom-left", duration: 2500 });
+        title = "Image inconnue !";
       }
-      return <img src={img} alt={techno.name} title={techno.name} key={index} />;
+      return <img src={img} alt={techno.name} title={title} key={index} />;
     })}
   </div>
 );

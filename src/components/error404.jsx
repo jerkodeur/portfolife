e775c6a1bb3dae24d28f55d9398825e98ae0cbec
jerkoLegion <jerome.potie@gmail.com>
2021-10-07 14:13:10 +0200

    Transform error404 page in functionnal component and adjust the container height

--- src/components/error404.jsx
+++ src/components/error404.jsx
@@ -1,43 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "@app/Header";
 import ScrollToTop from "react-scroll-up";
 import { FiChevronUp } from "react-icons/fi";
 
-class error404 extends Component {
-  render() {
-    return (
-      <>
-        <Header headerPosition="header--transparent" color="color-white" logo="logo-light" />
-        {/* Start Page Error  */}
-        <div className="error-page-inner bg_color--4">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="inner">
-                  <h1 className="title theme-gradient">404!</h1>
-                  <h3 className="sub-title">Page not found</h3>
-                  <span>The page you were looking for could not be found.</span>
-                  <div className="error-button">
-                    <a className="rn-button-style--2 btn-solid" href="/">
-                      Back To app
-                    </a>
-                  </div>
-                </div>
+const error404 = () => (
+  <>
+    <Header headerPosition="header--transparent" color="color-white" logo="logo-light" />
+    {/* Start Page Error  */}
+    <div className="error-page-inner bg_color--4">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="inner">
+              <h1 className="title theme-gradient">404!</h1>
+              <h3 className="sub-title">Page inexistante</h3>
+              <span>La page que vous recherchez n'a pas été trouvée.</span>
+              <div className="error-button">
+                <a className="rn-button-style--2 btn-solid" href="/">
+                  Revenir sur la page d'accueil
+                </a>
               </div>
             </div>
           </div>
         </div>
-        {/* End Page Error  */}
+      </div>
+    </div>
+    {/* End Page Error  */}
 
-        {/* Start Back To Top */}
-        <div className="backto-top">
-          <ScrollToTop showUnder={160}>
-            <FiChevronUp />
-          </ScrollToTop>
-        </div>
-        {/* End Back To Top */}
-      </>
-    );
-  }
-}
+    {/* Start Back To Top */}
+    <div className="backto-top">
+      <ScrollToTop showUnder={160}>
+        <FiChevronUp />
+      </ScrollToTop>
+    </div>
+    {/* End Back To Top */}
+  </>
+);
+
 export default error404;

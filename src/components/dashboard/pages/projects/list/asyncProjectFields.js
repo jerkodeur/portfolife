d64853d09791d850ad9a0005e8c535bc7445150e
jerkoLegion <jerome.potie@gmail.com>
2021-10-19 21:08:51 +0200

    Refactor the project list container

---
+++ src/components/dashboard/pages/projects/list/asyncProjectFields.js
@@ -0,0 +1,31 @@
+export const asyncProjectFields = [
+  {
+    id: "title",
+    placeholder: "titre"
+  },
+  {
+    id: "shortDescription",
+    placeholder: "Description courte"
+  },
+  {
+    id: "urlGithub",
+    placeholder: "Url Github",
+    type: "url"
+  },
+  {
+    id: "urlTest",
+    placeholder: "Url preview",
+    type: "url"
+  },
+  {
+    id: "nbImages",
+    type: "number",
+    min: 0,
+    max: 20,
+    defaultValue: 0
+  },
+  {
+    id: "imgPrefix",
+    placeholder: "Préfixe images"
+  }
+];
